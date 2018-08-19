110f37dea876e7622349a60a5eebff607b318cdd
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-19 20:09:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,22 @@ function getRepositories() {
    xhr.addEventListener("load", displayRepositories)
    xhr.open("Get", uri)
    xhr.send()
-     return false;   
+     return false;
+}
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText)
+  const repoList = "<ul>" + repos.map(repo => {
+    const dataUsername = 'data-username="' + repo.owner.login + '"'
+    const dataRepoName = 'data-repository="' + repo.name + '"'
+    return(`
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`
+          )
+  }).join('') + "</ul>";
+  document.getElementById("repositories").innerHTML = repoList
 }
